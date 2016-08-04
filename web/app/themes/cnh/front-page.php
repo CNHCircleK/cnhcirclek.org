3a74632e10405fcf09d70a155b61c62fe4807261
Joseph Le <josephle@me.com>
2016-08-03 23:53:12 -0700

    Changed template to load posts dynamically

--- web/app/themes/cnh/front-page.php
+++ web/app/themes/cnh/front-page.php
@@ -3,7 +3,11 @@ use Timber\Timber;
 
 if ( 'posts' == get_option( 'show_on_front' ) ) {
 	$context          = Timber::get_context();
-	$context['posts'] = Timber::get_posts();
+	$context['posts'] = array_merge( Timber::get_posts( [ 'post__in' => get_option( 'sticky_posts' ) ] ),
+		Timber::get_posts( [ 'category_name'  => 'news',
+		                     'post__not_in'   => get_option( 'sticky_posts' ),
+		                     'posts_per_page' => 2
+		] ) );
 	$templates        = [ 'front-page.twig' ];
 
 	Timber::render( $templates, $context );
