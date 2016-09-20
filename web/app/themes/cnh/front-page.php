ace4b16e9b6925e2eda2f529df06a15fe743c49b
Joseph Le <josephle@me.com>
2016-09-20 14:33:11 -0400

    Fix front page posts

--- web/app/themes/cnh/front-page.php
+++ web/app/themes/cnh/front-page.php
@@ -3,11 +3,7 @@ use Timber\Timber;
 
 if ( 'posts' == get_option( 'show_on_front' ) ) {
 	$context          = Timber::get_context();
-	$context['posts'] = array_merge( Timber::get_posts( [ 'post__in' => get_option( 'sticky_posts' ) ] ),
-		Timber::get_posts( [ 'category_name'  => 'news',
-		                     'post__not_in'   => get_option( 'sticky_posts' ),
-		                     'posts_per_page' => 2
-		] ) );
+	$context['posts'] = Timber::get_posts();
 	$templates        = [ 'front-page.twig' ];
 
 	Timber::render( $templates, $context );
