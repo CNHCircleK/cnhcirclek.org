b2847674e9044fca8ab18559ec5b94a0c1aad1ce
Joseph Le <josephle@me.com>
2016-09-30 21:05:47 -0700

    Limit number of posts on front page and pull from news category

--- web/app/themes/cnh/front-page.php
+++ web/app/themes/cnh/front-page.php
@@ -3,7 +3,19 @@ use Timber\Timber;
 
 if ( 'posts' == get_option( 'show_on_front' ) ) {
 	$context          = Timber::get_context();
-	$context['posts'] = Timber::get_posts();
+	$context['posts'] = Timber::get_posts(
+		array(
+			'post_type' => 'post',
+			'numberposts' => 2,
+			'tax_query' => array(
+				array(
+					'taxonomy' => 'category',
+					'field' => 'slug',
+					'terms' => 'news'
+				)
+			)
+		)
+	);
 	$templates        = [ 'front-page.twig' ];
 
 	Timber::render( $templates, $context );
