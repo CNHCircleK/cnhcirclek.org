0a3c12aa4ef3ab2b26c02f206fdc7d8bdce76709
Joseph Le <josephle@me.com>
2017-04-02 22:36:39 -0700

    Added pagination to category pages

--- web/app/themes/cnh/category.php
+++ web/app/themes/cnh/category.php
@@ -11,4 +11,5 @@
 $templates = array( 'category.twig' );
 $context = Timber::get_context();
 $context['posts'] = Timber::get_posts();
+$context['pagination'] = Timber::get_pagination();
 Timber::render( $templates, $context );
