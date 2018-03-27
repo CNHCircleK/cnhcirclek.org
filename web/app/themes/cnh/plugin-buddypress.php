3554c7e21c88399a280674898f0c6b9693765aa3
Denny Cao <dennycao202@yahoo.com>
2018-03-26 22:16:06 -0700

    Remove BuddyPress template var

--- web/app/themes/cnh/plugin-buddypress.php
+++ web/app/themes/cnh/plugin-buddypress.php
@@ -5,5 +5,4 @@ $context = Timber::get_context();
 global $wp_query;
 $wp_query->in_the_loop = true; // Strange Hack due to Timber... see Buddypress - bp_do_theme_compat()
 $context['post']       = new TimberPost();
-$templates = [ 'plugin-buddypress.twig' ];
 Timber::render( 'page.twig', $context );
